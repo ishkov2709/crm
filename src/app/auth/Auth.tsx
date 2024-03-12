245be4c2d0500130849ce51e647a5f1d39f117b8
ishkov2709 <dmitryishkov27@gmail.com>
2024-03-12 23:06:18 +0200

    add auth page

---
+++ src/app/auth/Auth.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { useMutation } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+import { IAuthForm } from '@/types/auth.types'
+
+import { DASHBOARD_PAGES } from '@/config/pages-url.config'
+
+import { authService } from '@/api/services/auth.service'
+
+export function Auth() {
+	const { register, handleSubmit, reset } = useForm<IAuthForm>({
+		mode: 'onChange'
+	})
+
+	const [isLoginForm, setIsLoginForm] = useState(false)
+	const { push } = useRouter()
+
+	const { mutate } = useMutation({
+		mutationKey: ['auth'],
+		mutationFn: (data: IAuthForm) =>
+			authService.main(isLoginForm ? 'login' : 'register', data),
+		onSuccess() {
+			toast.success('Successfully login!')
+			reset()
+			push(DASHBOARD_PAGES.HOME)
+		}
+	})
+
+	const onSubmit: SubmitHandler<IAuthForm> = data => {
+		mutate(data)
+	}
+
+	return <div>Auth</div>
+}
